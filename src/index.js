adca8154a03b97a2202b77180884db86c8da630c
Bozhidar Zahariev <bozhidar_zahariev@hotmail.com>
2019-09-21 13:16:07 +0100

    update

--- src/index.js
+++ src/index.js
@@ -127,10 +127,7 @@ import './index.css';
   
   // ========================================
   
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+  ReactDOM.render(<Game />, document.getElementById("root"));
 
   function calculateWinner(squares) {
     const lines = [
